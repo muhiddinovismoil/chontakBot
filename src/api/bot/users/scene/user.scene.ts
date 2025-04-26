cba17c4165aebf8e3799a7e1a0cf7a922c61f31e
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-04-26 10:27:57 +0500

    all media handlers updated

--- src/api/bot/users/scene/user.scene.ts
+++ src/api/bot/users/scene/user.scene.ts
@@ -20,38 +20,60 @@ export class BeginScene {
   }
   @On('audio')
   async onAudioHandler(@Ctx() ctx: general.ContextType) {
+    const audio = (ctx.update as any).message.audio;
+    console.log(audio?.file_id);
     ctx.reply('audio');
+    // ctx.scene.enter('AskKeyScene');
   }
   @On('animation')
   async onAnimationHandler(@Ctx() ctx: general.ContextType) {
-    ctx.reply('video');
+    const animation = (ctx.update as any).message.animation;
+    console.log(animation?.file_id);
+    ctx.reply('animation');
+    // ctx.scene.enter('AskKeyScene');
   }
 
   @On('document')
   async onFileHandler(@Ctx() ctx: general.ContextType) {
+    const document = (ctx.update as any).message.document;
+    console.log(document?.file_id);
     ctx.reply('file');
+    // ctx.scene.enter('AskKeyScene');
   }
 
   @On('video')
   async onVideoHandler(@Ctx() ctx: general.ContextType) {
+    const video = (ctx.update as any).message.video;
+    console.log(video?.file_id);
     ctx.reply('video');
+    // ctx.scene.enter('AskKeyScene');
   }
 
   @On('sticker')
   async onStickerHandler(@Ctx() ctx: general.ContextType) {
-    ctx.reply('sticker');
+    const sticker = (ctx.update as any).message.sticker;
+    const { file_id } = sticker;
+    ctx.reply(sticker);
+    // ctx.scene.enter('AskKeyScene');
   }
 
   @On('voice')
   async onVoiceHandler(@Ctx() ctx: general.ContextType) {
-    ctx.reply('voice');
+    const voice = (ctx.update as any).message.voice;
+    const { file_id } = voice;
+    ctx.session.lastMessage = ctx.message?.message_id;
+    ctx.session.lastText = file_id;
+    // ctx.scene.enter('AskKeyScene');
   }
 
   @On('location')
   async onLocationHandler(@Ctx() ctx: general.ContextType) {
     const location = (ctx.update as any).message.location;
+    const message = ctx.message?.message_id;
     const { latitude, longitude }: general.LocationI = location;
-    // console.log()
+    ctx.session.lastText = `${latitude}_${longitude}`;
+    ctx.session.lastMessage = message;
+    // ctx.scene.enter('AskKeyScene');
   }
 }
 @Scene('AskKeyScene')
