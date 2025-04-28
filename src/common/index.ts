8c2e900601eabb7705afa470596b075a39f85ace
Ismoil Muhiddinov <muhiddinovismoil2@gmail.com>
2025-04-28 12:24:06 +0500

    switch cases moved to function

--- src/common/index.ts
+++ src/common/index.ts
@@ -4,3 +4,4 @@ export * from './general/keyboard';
 export * from './general/message';
 export * from './template/reply.template';
 export * from './enum';
+export * from './function';
--- src/common/template/reply.template.ts
+++ src/common/template/reply.template.ts
@@ -2,5 +2,5 @@ export function createTemplate(data: { key: string; content: string }) {
   return `Kalit so'z: <b>${data.key}</b>\n\nTekst : <b>${data.content}</b>`;
 }
 export function KeyWordTemplate(text: string) {
-  return `Kalit so'z: ${text}`;
+  return `Kalit so'z: <b>${text}</b>`;
 }
